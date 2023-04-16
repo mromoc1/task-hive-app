1ce75f2a16e4cdd2ed4e73b22a12d4b9811f9d62
mromoc1 <mromo@alumnosuls.cl>
2023-04-15 21:22:56 -0400

    Inicializacion del proyecto

---
+++ src/main/java/com/hhdevs/taskhiveapp/TaskHiveAppApplication.java
@@ -0,0 +1,13 @@
+package com.hhdevs.taskhiveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskHiveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskHiveAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhdevs/taskhiveapp/TaskHiveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhdevs.taskhiveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskHiveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
